71064a890cfaa4273820f6d76b9e58bca5608375
Anthony Browness <demos74dx@gmail.com>
2014-02-22 18:49:49 -0700

    Putting my initials on this commit: AJB

--- js/canvas.js
+++ js/canvas.js
@@ -1,11 +1,25 @@
-$(document).ready(function() {
-  $('a[rel=popover]').click(function() {
-    $(this).popover('toggle');
-  });
-  $('a[rel=popover]').hover(function() {
-    $(this).popover('show');
-  }, function() {
-    $(this).popover('hide');
-  });
-});
+$(function(){
 
+	var options = {
+        html : true, 
+		animation: false,
+		template: '<div class="popover"><div class="arrow"></div><div class="popover-inner"><div class="popover-content"><p></p></div></div></div>',
+		placement: function (context, source) {
+			var position = $(source).position();
+
+			if (position.left > 515) {
+				return "left";
+			}
+			if (position.left < 515) {
+				return "right";
+			}
+			if (position.top < 110){
+				return "bottom";
+			}
+			return "top";
+		},
+		trigger: "hover"
+	};
+
+	$('[rel=popover]').popover(options);
+});		
